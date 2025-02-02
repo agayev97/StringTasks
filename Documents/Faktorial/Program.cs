97a4a041a5528592ee7d7f6e372a747a91f98f92
Tural Agayev <tuaralagayev@gmail.com>
2025-02-02 16:19:44 +0400

    Kitabxana layihesi elave olundu

---
+++ Documents/Faktorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faktorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a - ni daxil edin:");
+
+            int a = int.Parse(Console.ReadLine());
+
+            int faktorial = Faktorial(a);
+            Console.WriteLine($"{a}! = {faktorial}");
+
+            Console.ReadKey();
+        } 
+
+       static int Faktorial(int x)
+        {
+            if (x == 0) return 1;
+            return x * Faktorial(x - 1);
+        }
+    }
+}
---
+++ Documents/LibraryApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp
+{
+    internal class Program
+    {
+        public class Book
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public int PublishedYear { get; set; }
+            public bool IsAvailable { get; set; } = true;
+        }
+
+        public class Member
+        {
+            public int Id { get; set; }
+            public string FullName { get; set; }
+            public string Email { get; set; }
+            public string PhoneNumber { get; set; }
+            public DateTime MembershipDate { get; set; } = DateTime.Now;
+        }
+
+        public class Lease
+        {
+            public int Id { get; set; }
+            public int BookId { get; set; }
+            public int MemberId { get; set; }
+            public DateTime LeaseDate { get; set; } = DateTime.Now;
+            public DateTime? ReturnDate { get; set; }
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
